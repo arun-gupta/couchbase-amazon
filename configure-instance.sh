f4b040f6067d119fb8703ef80aa2c387cc37026d
Arun Gupta <arun.gupta@gmail.com>
2016-05-13 16:39:29 -0700

    configuring multiple instances

--- configure-instance.sh
+++ configure-instance.sh
@@ -1,8 +1,8 @@
 for ip in "$@"
 do
-	curl -v -X POST http://$1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
-	curl -v http://$1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
-	curl -v http://$1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
-	curl -v -u Administrator:password -X POST http://$1:8091/sampleBuckets/install -d '["travel-sample"]'
+	curl -v -X POST http://$ip:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
+	curl -v http://$ip:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
+	curl -v http://$ip:8091/settings/web -d port=8091 -d username=Administrator -d password=password
+	curl -v -u Administrator:password -X POST http://$ip:8091/sampleBuckets/install -d '["travel-sample"]'
 done
 
